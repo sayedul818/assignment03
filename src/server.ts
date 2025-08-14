b3cc9203f6455600d646fb032f772b33fe2360ef
SAYED <sayedulsayed818@gmail.com>
2025-08-15 04:34:17 +0600

    updated

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import {Server} from 'http'
 import app from './app'
 import mongoose from 'mongoose'
-
+import dotenv from 'dotenv'
 let server : Server
-const PORT = 6500;
+dotenv.config()
+const PORT = process.env.PORT || 6500;
 async function main(){
     try {
-        await mongoose.connect('mongodb+srv://sayed:12345@cluster1.bmyln.mongodb.net/advanced-noteApp?retryWrites=true&w=majority&appName=Cluster1');
+        await mongoose.connect(process.env.MONGO_URI!);
         console.log("connected to mongodb using mongoose successfully");
         server=app.listen(PORT,()=>{
             console.log(`app is listening on port ${PORT}` )
